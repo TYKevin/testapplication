8101a2b2f3e052a5edbe8be481dd670b7af4d14e
TongYu <tongyu@gegebox.com>
2022-09-15 14:52:14 +0800

    init

---
+++ app/src/main/java/com/geely/testapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.geely.testapplication;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private AppBarConfiguration appBarConfiguration;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
